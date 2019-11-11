36f9d74908677cf12a7b3e95a50a3dc62f43970c
pabloPeralta <paps21.gol@gmail.com>
2019-11-11 16:36:39 -0600

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    this.items.sort((a,b) => {
+      return a - b;
+    });
 
-  get(pos) {}
+  }
 
-  max() {}
+  get(pos) {
+    if( pos > this.items.length ){
+      return new Error("OutOfBounds");
+    }
+    return this.items[this.items.length -1 ]
+  }
 
-  min() {}
+  max() {
+    if(this.items.length === 0 ){
+      return new Error("EmptySortedList");
+    }else {
+      return this.items[this.items.length -1];
+    }
+  }
 
-  sum() {}
+  min() {
+    if(this.items.length === 0 ){
+      return new Error("EmptySortedList");
+    }else {
+      return this.items[0]; //ultimo elemento 
+    }
+  }
 
-  avg() {}
+  sum() {
+    if(this.items.length === 0){
+      return 0
+    }
+    return this.items.reduce((acc,cur) => {
+      return acc + cur;
+    });
+  }
+
+  avg() {
+    if(this.items.length === 0 ){
+      return new Error("EmptySortedList");
+    }else {
+      return this.sum() / this.items.length;
+    }
+  }
 }
 
 module.exports = SortedList
